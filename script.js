652e76176fc5b2fbc337f666f96d450a32f7ecfe
ldolohov <ldolohov@gmail.com>
2023-07-15 22:00:58 +0900

    set custom validity for input

--- script.js
+++ script.js
@@ -5,9 +5,17 @@ const taskManager = new TaskManager();
 const form = document.querySelector('#task-form');
 
 taskManager.render();
+
+const taskInput = document.querySelector('#task-input');
+taskInput.setCustomValidity('Even one word...');
+taskInput.addEventListener('input', () => {
+    taskInput.setCustomValidity('');
+  });
+// Add an 'onsubmit' event listener
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     const taskName = form.querySelector('#task-input').value;
+
     taskManager.addTask(taskName);
     taskManager.render();
     form.querySelector('#task-input').value = '';
